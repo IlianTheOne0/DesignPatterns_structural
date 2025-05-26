3f888c7b792d4f2df072684e56ffbc28eb1d465d
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-05-26 18:47:02 +0300

    Completed coding of the adapter structural design pattern

---
+++ ClassLibrary/Adapter.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibrary.StructuralPatterns.Adapter;
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class OldLogger : ILogger
+{
+    public void Log(string message) { Console.WriteLine($"[Old logger] {message}"); }
+}
+
+public class LoggerAdapter : ILogger
+{
+    private readonly ILogger _logger;
+
+    public LoggerAdapter(ILogger logger) => _logger = logger;
+
+    public void Log(string message) { _logger.Log($"[New logger] {message}"); } 
+}
--- ConsoleApplication/Main.cs
+++ ConsoleApplication/Main.cs
@@ -1,6 +1,15 @@
-﻿namespace ConsoleApplication;
+﻿namespace ClassLibrary.StructuralPatterns.Adapter;
 
-internal class Program
+internal class Client
 {
-    static void Main(string[] args) { }
+    public static void Main(string[] args)
+    {
+        ILogger logger0 = new OldLogger();
+        Console.WriteLine("App: Launched with the OldLogger.");
+        logger0.Log("Hello world!");
+
+        ILogger logger1 = new LoggerAdapter(logger0);
+        Console.WriteLine("App: Launched with the addoted logger.");
+        logger1.Log("New hello world!");
+    }
 }
